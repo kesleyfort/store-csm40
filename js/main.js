5ec25e050e5823dd820af5229c6c3598c6f42895
Kesley Fortunato Alves <kesleyfortunato@outlook.com>
2022-05-11 11:10:11 -0300

    Menus laterais funcionando. Site parcialmente estilizado. Puto com essa merda por tá tendo que fazer tudo na unha. Falta: - Fale conosco - Produtos - Menu Categorias - Footer - Cadastro Usuário

--- js/main.js
+++ js/main.js
@@ -31,3 +31,32 @@ function openLoginNav() {
 function closeLoginNav() {
   document.getElementById("loginNav").style.width = "0";
 }
+function mascaraCpf(i){
+
+  var v = i.value;
+
+  if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+    i.value = v.substring(0, v.length-1);
+    return;
+  }
+
+  i.setAttribute("maxlength", "14");
+  if (v.length === 3 || v.length === 7) i.value += ".";
+  if (v.length === 11) i.value += "-";
+
+}
+function mascaraCep(i){
+
+  var v = i.value;
+
+  if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+    i.value = v.substring(0, v.length-1);
+    return;
+  }
+
+  i.setAttribute("maxlength", "10");
+  if (v.length === 2) i.value += ".";
+  if (v.length === 6) i.value += "-";
+
+}
+
